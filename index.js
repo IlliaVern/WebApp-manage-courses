7b76c34c3e2d81cf027ebb0e379054b532d45bc3
Illia Vernygora <illiavernigora@gmail.com>
2020-06-14 19:16:23 +0300

    CSRF protection added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const csrf = require('csurf')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const exphbs = require('express-handlebars')
@@ -42,6 +43,7 @@ app.use(session({
     saveUninitialized: false,
     store: store
 }))
+app.use(csrf())
 app.use(varMiddleware)
 app.use(userMiddleware)
 
