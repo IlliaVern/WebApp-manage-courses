1660dee9749178144b135b8511f5e64387ccb03d
Illia Vernygora <illiavernigora@gmail.com>
2020-06-14 19:42:22 +0300

    login/reg error displayed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const csrf = require('csurf')
+const flash = require('connect-flash')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const exphbs = require('express-handlebars')
@@ -13,7 +14,6 @@ const coursesRoutes = require('./routes/courses')
 const ordersRoutes = require('./routes/orders')
 const cartRoutes = require('./routes/cart')
 const authRoutes = require('./routes/auth')
-// const User = require('./models/user')
 const varMiddleware = require('./middleware/variables')
 const userMiddleware = require('./middleware/user')
 
@@ -44,6 +44,7 @@ app.use(session({
     store: store
 }))
 app.use(csrf())
+app.use(flash())
 app.use(varMiddleware)
 app.use(userMiddleware)
 
--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,9 @@ const router = Router()
 router.get('/login', async (req, res) => {
     res.render('auth/login', {
         title: 'Authorization',
-        isLogin: true
+        isLogin: true,
+        loginError: req.flash('loginError'),
+        registerError: req.flash('registerError')
     })
 })
 
@@ -34,9 +36,11 @@ router.post('/login', async (req, res) => {
                     res.redirect('/')
                 })
             } else {
+                req.flash('loginError', 'Wrong password')
                 res.redirect('/auth/login#login')
             }
         } else {
+            req.flash('loginError', 'User does not exists')
             res.redirect('/auth/login#login')
         }
     } catch (e) {
@@ -49,6 +53,7 @@ router.post('/register', async (req, res) => {
         const {email, name, password, repeat} = req.body
         const candidate = await User.findOne({email})
         if (candidate) {
+            req.flash('registerError', 'User with entered email has been already in use')
             res.redirect('/auth/login#register')
         } else {
             const hashPassword = await bcrypt.hash(password, 10)
